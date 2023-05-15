79c9bb961bfc1294acf1914d1df0931f9a8aa812
Kyle Kimsey <kael29lv@gmail.com>
2023-05-15 14:02:41 -0700

    Update healthcheck_2023.sh
    Fixed syntax issue.

--- unRAID/Grafana-Unraid-Stack/healthcheck_2023.sh
+++ unRAID/Grafana-Unraid-Stack/healthcheck_2023.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start
-echo "---- $(date "+%d.%m.%Y_%T"): Healthcheck Start ----"
+echo "----- $(date "+%d.%m.%Y_%T"): Healthcheck Start -----"
 
 # Check if the ENV variable for a new Log Folder is set and replace it, otherwise use default.
 logout_folder=${LOG_FOLDER:-"/config"}
